8ef76b4ee840890a1fd9494a91e9b09440942864
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2022-12-24 15:35:37 +0545

    Product Rating Added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
+import PropTypes from 'prop-types'
 
 const Product = ({ product }) => {
     return (
@@ -16,9 +18,7 @@ const Product = ({ product }) => {
                 </a>
 
                 <Card.Text>
-                    <div className='my-3'>
-                        {product.rating} from {product.numReviews} reviews
-                    </div>
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
                 </Card.Text>
 
                 <Card.Text as='h3'>
@@ -27,9 +27,17 @@ const Product = ({ product }) => {
 
             </Card.Body>
         </Card>
-
-
     )
+}
+
+Rating.defaultProps = {
+    color: '#f8e825'
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
 }
 
 export default Product
