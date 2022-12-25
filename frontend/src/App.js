009ad06b0ecd6ce9afc388e518620d4d098b31f9
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2022-12-25 21:05:26 +0545

    Product Screen Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Container } from "react-bootstrap"
 import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
