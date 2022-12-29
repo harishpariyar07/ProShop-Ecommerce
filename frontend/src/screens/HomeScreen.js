4da0f9ae508314ec1b49af766c272360f3e90029
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2022-12-29 21:10:18 +0545

    Implemented Redux Logic in Homescreen with useDispatch and useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,40 @@
-import Product from '../components/Product'
-import React, { useEffect, useState } from 'react'
-import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
+import Product from "../components/Product";
+import React, { useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productsActions";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList);
+
+    const { loading, error, products } = productList;
+
     useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await axios.get('/api/products')
-            setProducts(res.data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts());
+    }, [dispatch]);
 
     return (
         <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map((product) => {
-                    return (<Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} key={product._id} />
-                    </Col>)
-                })}
-            </Row>
+            <h1 className='my-3'>Latest Products</h1>
+            {loading ? (
+                <h2>Loading...</h2>
+            ) : error ? (
+                <h3>{error}</h3>
+            ) : (
+                <Row>
+                    {products.map((product) => {
+                        return (
+                            <Col sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} key={product._id} />
+                            </Col>
+                        );
+                    })}
+                </Row>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
