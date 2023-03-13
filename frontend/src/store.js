fb9356738f3ff93e1ab589d24181731bd8203182
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-03-13 17:29:51 +0530

    updated redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,22 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 })
 
 const middleware = [thunk]
 
-const initialState = {}
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage }
+}
 
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
