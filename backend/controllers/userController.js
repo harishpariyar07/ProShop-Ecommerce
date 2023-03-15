e52718f791d50ab769b9c688b33a00846dce85b7
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-03-15 12:01:33 +0530

    Updated user controller to register new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,4 +46,41 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+// @desc    Register a new user
+// @route   POST /api/users/
+// @access  Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExits = await User.findOne({ email })
+
+    if (userExits) {
+        res.status(404)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    console.log(user)
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }
+    else {
+        res.status(401)
+        throw new Error('User not found')
+    }
+})
+
+export { authUser, getUserProfile, registerUser }
