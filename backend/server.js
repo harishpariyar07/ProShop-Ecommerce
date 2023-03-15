5b1ae4d6775a824740444ee5ea223dc87541c7d3
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-03-15 07:22:31 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 connectDB()
@@ -11,11 +12,14 @@ dotenv.config()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("API is running... Go to /api/products to fetch products data")
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 
 app.use(notFound)
