43f2ee62305f56c9403c1d51385a3c17e9ab3db4
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-04-11 11:28:55 +0530

    fix: fixed route for login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -19,7 +19,8 @@ const LoginScreen = () => {
 
     useEffect(() => {
         if (userInfo) {
-            navigate(`/${redirect}`)
+            console.log(redirect)
+            navigate(`${redirect}`)
         }
     }, [navigate, userInfo, redirect])
 
