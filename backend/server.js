cc31dc6e296bb6cc2217e73a96a10fd34aa3bb93
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-05-10 00:51:38 +0530

    Cors added

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import cors from 'cors'
 
 connectDB()
 
 dotenv.config()
 
 const app = express()
+
+app.use(cors({
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}))
 
 app.use(express.json())
 
@@ -22,7 +29,10 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
