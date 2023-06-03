7a7efd47b53044a5e85d71a539dcfc2c2b56680b
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-03 11:54:54 +0545

    Added morgan for logging http requests

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,17 @@ import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import cors from 'cors'
+import morgan from 'morgan'
 
 connectDB()
 
 dotenv.config()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(cors({
     origin: process.env.ALLOWED_CLIENTS.split(',')
