6ea0f2034b2f6a7def90c2cf6a391a0373c5c66a
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-03 11:52:52 +0545

    Product review controller and route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,14 +1,13 @@
 import express from "express";
 import asyncHandler from "express-async-handler"
 import Product from '../models/productModel.js'
-import { getProducts, getProductById, updateProduct, createProduct } from "../controllers/productControllers.js";
-import { deleteProduct } from "../controllers/productControllers.js";
+import { getProducts, getProductById, updateProduct, createProduct, deleteProduct, createProductReview } from "../controllers/productControllers.js";
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
-
+router.route('/:id/review').post(protect, createProductReview)
 router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default router
