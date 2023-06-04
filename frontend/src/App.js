52bcef29bab4c821e21f4227ec4c230bc6b2c267
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2023-06-04 08:20:16 +0545

    Route added for search query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,6 +40,7 @@ function App() {
             <Route path='/admin/productlist' element={<ProductListScreen />} />
             <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
             <Route path='/admin/orderlist' element={<OrderListScreen />} />
+            <Route path='/search/:keyword' element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
